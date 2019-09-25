9aab24992b83a6b5b4950926e4add7451379fc46
Rajdeep Sahoo <rajdeepsahoo@nuclaysolutions.com>
2019-09-25 14:47:12 +0530

    Initial Commit

---
+++ HeadyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadyMovies
+//
+//  Created by Rajdeep Sahoo on 25/09/19.
+//  Copyright © 2019 Rajdeep Sahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
